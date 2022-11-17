3147dbe38f50281f6fac53cd3c737319fb524458
L.B <mrlawrencelane@gmail.com>
2022-11-17 17:59:44 -0500

    update

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,19 +1,7 @@
  <script>
    import { supabase } from '../supabase.js';
-     import { Navbar, NavBrand, NavHamburger, NavUl, NavLi, Chevron, MegaMenu } from 'flowbite-svelte'
-  let menu = [
-    {name: 'About us', href: '/about'},
-    {name: 'Blog', href: '/blog'},
-    {name: 'Contact us', href: '/contact'},
-    {name: 'Library', href: '/library'},
-    {name: 'Newsletter', href: '/news'},
-    {name: 'Support Center', href: '/support'},
-    {name: 'Resources', href: '/resource'},
-    {name: 'Playground', href: '/play'},
-    {name: 'Terms', href: '/terms'},
-    {name: 'Pro Version', href: '/pro'},
-    {name: 'License', href: '/license'},
-  ];
+     import { Navbar, NavBrand, NavHamburger, NavUl, NavLi} from 'flowbite-svelte'
+
 
    const logout = () => {
       supabase.auth.signOut();
@@ -30,10 +18,6 @@
   <NavHamburger on:click={toggle} />
   <NavUl {hidden}>
     <NavLi href="/">Home</NavLi>
-    <NavLi><Chevron aligned>Mega menu</Chevron></NavLi>
-    <MegaMenu items={menu} let:item>
-      <a href={item.href} class="hover:text-blue-600 dark:hover:text-blue-500">{item.name}</a>
-    </MegaMenu>
     <NavLi href="/" on:click={logout}>Log Out</NavLi>
   </NavUl>
 </Navbar>
