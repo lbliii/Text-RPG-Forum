5e4d077c7aef1248b7aec6a83ae677eccfc205c8
L.B <mrlawrencelane@gmail.com>
2023-02-15 21:26:14 -0500

    cleanup

--- src/routes/character/create/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    import CharacterForm from '../../../components/CharacterForm.svelte';
-</script>
-
-<main>
-    <h1 class="text-3xl font-bold dark:text-white text-center">Create a Character</h1>
-    <CharacterForm />
-
-</main>
--- src/routes/thread/[slug]/+page.svelte
+++ src/routes/thread/[slug]/+page.svelte
@@ -1,16 +1,24 @@
 <script>
-  import {Avatar, Card, Heading, Hr, Modal, P} from 'flowbite-svelte'
+  import {Avatar, Card, Heading, Hr, Modal, P, Badge} from 'flowbite-svelte'
+  import { forumStore } from '../../../stores/forumStore.js';
   import Posts from '../../../components/Posts.svelte';
 
   export let data;
   let thread = data;
+  let forum = {};
+
+  $: forum = $forumStore.find(forum => forum.id === thread.topic_id);
 
 
 </script>
 
 <section>
   
-  <Heading tag="h1" class="mb-2">{thread.title}</Heading>
+  <div class="text-center">
+    <Heading tag="h1" class="my-3">{thread.title}</Heading>
+    <Badge href="/forum/{forum.id}">{forum.title}</Badge>
+  </div>
+  
 
   <Posts thread={thread}></Posts>
   
