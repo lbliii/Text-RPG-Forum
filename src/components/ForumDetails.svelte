178aade4d010fb7ee0781f22dcbb4b14ab015f16
L.B <mrlawrencelane@gmail.com>
2023-02-27 13:08:13 -0500

    checkpoint

---
+++ src/components/ForumDetails.svelte
@@ -0,0 +1,14 @@
+<script>
+    import { Heading} from 'flowbite-svelte';
+
+    export let forum
+
+</script>
+
+
+{#if forum}
+    <div class="my-6">
+        <Heading tag="h1" class="my-3 text-center text-white">{forum.title} Threads</Heading>
+        <Heading tag="h2" class="my-3 text-center text-white">{forum.description}</Heading>
+    </div>
+{/if}
--- src/routes/forum/[slug]/+page.js
+++ src/routes/forum/[slug]/+page.js
@@ -1,13 +1,7 @@
 import { forumStore } from '../../../stores/forumStore.js';
-import { handleError } from '../../../shared/helpers.js';
 
 export async function load({ params }) {
 	const id = params.slug;
 
-	try {
-		return forumStore.fetchForum(id);
-	
-	} catch (error) {
-		handleError(error);
-	}
+	return forumStore.fetchForum(id);
 }
