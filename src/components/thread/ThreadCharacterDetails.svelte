87a4aff97687fdc0bb91058be2b25d4a0b421855
L.B <mrlawrencelane@gmail.com>
2023-03-11 14:44:59 -0500

    checkpoint

--- src/components/thread/ThreadCharacterDetails.svelte
+++ src/components/thread/ThreadCharacterDetails.svelte
@@ -1,13 +1,12 @@
 <script>
 	import { threadCharactersStore } from '../../stores/threadCharactersStore.js';
-	import { characterStore } from '../../stores/characterStore.js';
 	import { Avatar } from 'flowbite-svelte';
 
 	export let thread;
+	
 
 	async function loadThreadCharacters() {
-  		let characterIds = await threadCharactersStore.fetchThreadCharacters(thread.id).then(data => data.map(link => link.character_id));
-  		let characters = await Promise.all(characterIds.map(characterId => characterStore.fetchCharacter(characterId)));
+  		let characters = await threadCharactersStore.fetchThreadCharacters(thread.id);
   	return characters;
 }	
 </script>
